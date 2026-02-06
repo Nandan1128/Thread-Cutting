12d731694fe69c995a107db13cf182581da1d94e
Nandan1128 <gogarinandan1128@gmail.com>
2026-02-06 21:50:38 +0530

    final web build1

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -26,7 +26,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "767e8e9bedaa86328c930cf421d3726d
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "73adb6e1c28a093906258ea01e3bfa50",
+"flutter_bootstrap.js": "d896490ccea1f5c090f01d9da4d59c71",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
